facc27dcc9cbd248b73d45494288b0f72f437c8b
Querenmoreiradev <codewithqueren@gmail.com>
2026-04-04 20:52:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// DIGITAÇÃO
-const text = "Olá, eu sou Quéren Moreira";
-let i = 0;
-
-function typing(){
-if(i < text.length){
-document.getElementById("typing").innerHTML += text.charAt(i);
-i++;
-setTimeout(typing, 50);
-}
-}
-typing();
-
-
-// SCROLL ANIMATION
+// scroll reveal
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", ()=>{
@@ -25,14 +11,48 @@ el.classList.add("active");
 });
 
 
-// PARTÍCULAS
+// partículas
 tsParticles.load("particles-js", {
 particles: {
-number: { value: 60 },
+number: { value: 50 },
 color: { value: ["#ff007f", "#8a2be2"] },
-shape: { type: "circle" },
-opacity: { value: 0.5 },
-size: { value: 4 },
+size: { value: 5 },
 move: { enable: true, speed: 1 }
+}
+});
+const text1 = "Olá, eu sou Quéren Moreira";
+const text2 = "Desenvolvedora Front-End focada na criação de interfaces modernas, responsivas e funcionais, com interesse em automação e eficiência de sistemas.";
+
+let i = 0;
+let j = 0;
+
+function typingName(){
+const el = document.getElementById("typing");
+
+if(el && i < text1.length){
+el.innerHTML += text1.charAt(i);
+i++;
+setTimeout(typingName, 100);
+}else{
+typingSubtitle();
+}
+}
+
+function typingSubtitle(){
+const el = document.getElementById("subtitle");
+
+if(el && j < text2.length){
+el.innerHTML += text2.charAt(j);
+j++;
+setTimeout(typingSubtitle, 40);
+}
+}
+
+window.onload = () => {
+typingName();
+};window.addEventListener("load", ()=>{
+const loader = document.getElementById("loader");
+if(loader){
+loader.style.display = "none";
 }
 });
